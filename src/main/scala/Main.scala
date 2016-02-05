2e92eb9342e0ff9ecd16a188fefd3620f7143422
ksinb <61133806+ksinb@users.noreply.github.com>
2016-02-05 17:02:14 +0900

    Rejected commit: Default

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,13 @@
 /**
   * Created by Help Desk on 2016/02/05.
+  *
+  * name := "HelloTest"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4"
+
   */
 
 object Main {
